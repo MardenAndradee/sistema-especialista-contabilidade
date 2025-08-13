ada5ff4ca327e02fbe7de7cdab2a49205affb2c7
MardenAndradee <marden.andrade1@gmail.com>
2025-08-13 20:00:02 -0300

    Initial commit

---
+++ src/main/java/com/mofc/secontabil/SecontabilApplication.java
@@ -0,0 +1,13 @@
+package com.mofc.secontabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecontabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecontabilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mofc/secontabil/SecontabilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mofc.secontabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecontabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
